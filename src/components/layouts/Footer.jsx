d29824314f5d966c791c82c84e5ad3a766414f99
Dharmaraj Patidar <dharmarajpatidar8@gmail.com>
2026-01-03 18:24:10 +0530

    latest changes

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
                 <div className="relative z-10 px-10 pt-14 pb-10 grid grid-cols-1 md:grid-cols-4 gap-10">
                     <div>
                         <h2 className="text-white text-xl font-semibold mb-6 whitespace-pre-line">{footerConfig.newsletter.title}</h2>
-                        <div className="flex items-center border-b border-gray-600 pb-2 max-w-xs">
+                        <div className="flex justify-around items-center border-b border-gray-600 pb-2 max-w-xs">
                             <input type="email" placeholder={footerConfig.newsletter.placeholder}
                                 className="bg-transparent outline-none text-sm text-gray-300 placeholder-gray-500 flex-1"
                             />
